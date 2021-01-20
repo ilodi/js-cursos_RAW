a7b406ee57f8fa63727ab1a71afc104c3c089484
lodi <lodi.dev@outlook.com>
2021-01-20 11:00:43 -0600

    up

--- block-buster/src/components/app.js
+++ block-buster/src/components/app.js
@@ -1,15 +1,16 @@
 import { Component } from "../lib/react/index.js";
 import styled from "../lib/styled.js";
 import Header from "./header.js";
+import Actions from "./actions.js";
 
 const AppStyled = styled.div``;
 
 class App extends Component {
   render() {
     return AppStyled({
-      children: [
-          new Header()
-      ],
+      children: [new Header(),new Actions({
+        children:[]
+      })],
     });
   }
 }
---
+++ block-buster/src/components/search.js
@@ -0,0 +1,19 @@
+import { Component } from "../lib/react/index.js";
+import styled from "../lib/styled.js";
+{
+    /* <section class="wrapper">
+      <div class="actions">
+        <form action="" id="search-form">
+          <input placeholder="Escribe tu película favorita" name="title" type="Busca una pelicula" />
+          <button>Buscar</button>
+        </form>
+        <button id="recommended">Películas recomendadas</button>
+        <select name="" id="filter">
+          <option value="all">Todas</option>
+          <option value="most-valued">Más valoradas</option>
+          <option value="least-valued">Menos valoradas</option>
+        </select>
+      </div>
+    </section> */
+  }
+  
